86b9274a6ead5c4d779c92eab7e8748b7100f9b0
Khayal H <43758778+khmaxim@users.noreply.github.com>
2020-05-02 15:08:12 +0300

    Implement necessary functions

--- business.h
+++ business.h
@@ -6,39 +6,105 @@ using namespace std;
 class Business {
     string name;
     string address;
-    Person * owners;
+    Owner * owners;
+    Courier * couriers;
+    int numberOfOwners;
     public:
-        Business(string name, string address, Person * owners, int amountOfOwners) {
+        Business(string name, string address, Owner * owners, int numberOfOwners) {
             //The amount of “ownership” should be adjusted during construction as well.
             this->name = name;
             this->address = address;
-            this->owners = owners;
-            
+            this->owners = new Owner[numberOfOwners];
+            for (int i = 0; i < numberOfOwners; i++) {
+                this->owners[i] = owners[i];
+            }
+            //this->owners = owners;
+            this->numberOfOwners = numberOfOwners;
+            for (int i = 0; i < numberOfOwners; i++) {
+                this->owners[i].set_ownership(100 / numberOfOwners);
+            }
         }
-        void hire_courier(Courier * newcourier) {
+        void hire_courier(Courier newcourier) {
             //Insert courier to list/array
+            Courier * alt = new Courier(newcourier.getName(), newcourier.getSurname(), newcourier.getVehicleType());
+            if (couriers == NULL) {
+                couriers = alt;
+            } else {
+                Courier * traverse = couriers;
+                while(traverse->getNext() != NULL) {
+                    traverse = traverse->getNext();
+                }
+                traverse->setNext(alt);
+            }
         }
-        void fire_courier(Courier * target) {
+        void fire_courier(Courier target) {
             //Searches the courier and removes it from the list/array
             //If the courier does not exist it should print an error message
-            //You should overload == operator for Courier class which isrequired for the search part.
+            //You should overload == operator for Courier class which is required for the search part.
+            Courier * traverse = couriers;
+            Courier * tail = NULL;
+            while(traverse != NULL) {
+                if (*traverse == target) {
+                    if (tail == NULL) {
+                        couriers = traverse->getNext();
+                    } else {
+                        tail->setNext(traverse->getNext());
+                    }
+                    delete traverse;
+                    return;
+                }
+                tail = traverse;
+                traverse = traverse->getNext();
+            }
         }
         void list_couriers() {
             //This method prints all information (name, surname, vehicle_type) about all couriers to screen.
+            Courier * traverse = couriers;
+            while (traverse != NULL) {
+                cout << traverse->getName() << " " << traverse->getSurname() << " " << traverse->getVehicleType() << endl;
+                traverse = traverse->getNext();
+            }
         }
         void list_owners() {
             //This method prints all information (name, surname, ownership) about all owners to screen.
+            for (int i = 0; i < numberOfOwners; i++) {
+                cout << this->owners[i].getName() << " " << this->owners[i].getSurname() << " " << this->owners[i].get_ownership() << endl;
+            }
         }
-        void calculate_shipment_capacity() {
+        int calculate_shipment_capacity() {
             //This method should sum up total transportation capacity.
+            Courier * traverse = couriers;
+            int totalShipmentCapacity = 0;
+            while (traverse != NULL) {
+                string vehicleType = traverse->getVehicleType();
+                if (vehicleType.compare("car") == 0) {
+                    totalShipmentCapacity += 200;
+                } else if (vehicleType.compare("motorcycle") == 0) {
+                    totalShipmentCapacity += 35;
+                } else if (vehicleType.compare("bicycle") == 0) {
+                    totalShipmentCapacity += 10;
+                }
+                traverse = traverse->getNext();
+            }
+            return totalShipmentCapacity;
         }
         void operator () () {
             // it should print all information about the business (i.e., name, address, owners, couriers).
-            list_couriers();
+            cout << this->name << " " << this->address << endl;
             list_owners();
+            list_couriers();
         }
         Courier& operator [] (int index) {
             //it should return courier with the given index.
             //If the index is invalid, it should print an error message.
+            Courier * traverse = couriers;
+            for (int i = 0; i < index; i++) {
+                if (traverse->getNext() == NULL) {
+                    cout << "ERROR: Invalid courier index" << endl;
+                    return *traverse; // NEEDS A FIX
+                }
+                traverse = traverse->getNext();
+            }
+            return *traverse;
         }
 };
--- person.h
+++ person.h
@@ -11,29 +11,64 @@ class Person {
             this->name = name;
             this->surname = surname;
         }
+        string getName() {
+            return this->name;
+        }
+        string getSurname() {
+            return this->surname;
+        }
 };
 
 class Owner : public Person {
     int ownership; // as percentage
     public:
-        Owner();
-        Owner(string name, string surname):Person(name,surname){}
+        Owner():Person("undefined", "undefined"){
+            //ownership = 0;
+        }
+        Owner(string name, string surname):Person(name,surname){
+            //ownership = 0;
+        }
         void set_ownership(int percentage) {
-
+            this->ownership = percentage;
+        }
+        int get_ownership() {
+            return ownership;
         }
 };
 
 class Courier : public Person {
     string vehicle; // car (200), motorcycle (35), bicycle (10)
+    Courier * next;
+    string sur;
+    string namae;
     public:
         Courier(string name, string surname, string vehicle):Person(name,surname) {
+            this->next = NULL;
             if (vehicle.compare("car") == 0 || vehicle.compare("motorcycle") == 0 || vehicle.compare("bicycle") == 0) {
                 this->vehicle = vehicle;
             } else {
                 cout << "ERROR: Invalid Vehicle Type" << endl;
             }
+            this->sur = surname;
+            this->namae = name;
         }
-        bool operator==(const Courier& other) {
-            
+        bool operator==(const Courier& in_object) const {
+            if (this->sur.compare(in_object.sur) == 0) {
+                if (this->namae.compare(in_object.namae) == 0) {
+                    if (this->vehicle.compare(in_object.vehicle) == 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        Courier * getNext() {
+            return this->next;
+        }
+        void setNext(Courier * target) {
+            this->next = target;
+        }
+        string getVehicleType() {
+            return vehicle;
         }
 };
