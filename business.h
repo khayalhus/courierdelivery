19f11e1036bf382bb63f7396b8fde785134ba116
Khayal H <43758778+khmaxim@users.noreply.github.com>
2020-05-02 19:52:03 +0300

    Improve readability and error handling
    Add comments
    Change variable names to meaningful ones
    Change some conditions for invalid inputs

--- business.h
+++ business.h
@@ -4,95 +4,97 @@
 using namespace std;
 
 class Business {
-    string name;
-    string address;
-    Owner * owners;
-    Courier * couriers;
-    int numberOfOwners;
+    string name; // name of the business
+    string address; // physical address of the business
+    Owner * owners; // Dynamic Array holding info about owners
+    Courier * couriers; // Linked List holding couriers working for the business
+    int numberOfOwners; // amount of owners inside the Dynamic Array
     public:
         Business(string name, string address, Owner * owners, int numberOfOwners) {
             //The amount of “ownership” should be adjusted during construction as well.
-            this->name = name;
-            this->address = address;
-            this->owners = new Owner[numberOfOwners];
+            this->name = name; // sets name
+            this->address = address; // sets physical address
+            /*this->owners = new Owner[numberOfOwners];
             for (int i = 0; i < numberOfOwners; i++) {
                 this->owners[i] = owners[i];
-            }
-            //this->owners = owners;
-            this->numberOfOwners = numberOfOwners;
+            }*/
+            this->owners = owners; // sets pointer to parameter
+            this->numberOfOwners = numberOfOwners; // sets amount of Owner objects inside owners array
             for (int i = 0; i < numberOfOwners; i++) {
-                this->owners[i].set_ownership(100 / numberOfOwners);
+                this->owners[i].set_ownership(100 / numberOfOwners); // calculates and sets percentage of ownership for each owner
             }
         }
-        void hire_courier(Courier newcourier) {
+        void hire_courier(Courier courier) {
             //Insert courier to list/array
-            Courier * alt = new Courier(newcourier.getName(), newcourier.getSurname(), newcourier.getVehicleType());
-            if (couriers == NULL) {
-                couriers = alt;
+            Courier * newcourier = new Courier(courier.getName(), courier.getSurname(), courier.getVehicleType());
+            // allocate memory seperately for the newcourier
+            if (this->couriers == NULL) {
+                this->couriers = newcourier; // sets the first Courier for the Linked List
             } else {
-                Courier * traverse = couriers;
+                Courier * traverse = couriers; // used to traverse the Courier Linked List
                 while(traverse->getNext() != NULL) {
-                    traverse = traverse->getNext();
+                    traverse = traverse->getNext(); // traverses to the last element on the Linked List
                 }
-                traverse->setNext(alt);
+                traverse->setNext(newcourier); // adds target as last element's next element
             }
         }
         void fire_courier(Courier target) {
             //Searches the courier and removes it from the list/array
             //If the courier does not exist it should print an error message
             //You should overload == operator for Courier class which is required for the search part.
-            Courier * traverse = couriers;
-            Courier * tail = NULL;
+            Courier * traverse = couriers; // used to traverse the Courier Linked List
+            Courier * tail = NULL; // points to Courier before traverse in the Linked List
             while(traverse != NULL) {
-                if (*traverse == target) {
+                if (*traverse == target) { // calls overloaded operator== in order to test equivalency
                     if (tail == NULL) {
-                        couriers = traverse->getNext();
+                        couriers = traverse->getNext(); // fire the first courier in the Linked List
                     } else {
-                        tail->setNext(traverse->getNext());
+                        tail->setNext(traverse->getNext()); // fire the courier in the middle or at the end of the Linked List
                     }
-                    delete traverse;
-                    return;
+                    delete traverse; // delete the object for the desired Courier
+                    return; // return if the courier is found
                 }
                 tail = traverse;
-                traverse = traverse->getNext();
+                traverse = traverse->getNext(); // check next courier
             }
+            cout << "ERROR: Desired courier could not be found" << endl; // if courier can't be found, print an error message
         }
         void list_couriers() {
             //This method prints all information (name, surname, vehicle_type) about all couriers to screen.
-            Courier * traverse = couriers;
-            while (traverse != NULL) {
-                cout << traverse->getName() << " " << traverse->getSurname() << " " << traverse->getVehicleType() << endl;
-                traverse = traverse->getNext();
+            Courier * traverse = couriers; // used to traverse the Courier Linked List
+            while (traverse != NULL) { // stop if end of Linked List is reached
+                cout << traverse->getName() << " " << traverse->getSurname() << " " << traverse->getVehicleType() << endl; // calls parent member functions to print
+                traverse = traverse->getNext(); // print the next in the list
             }
         }
         void list_owners() {
             //This method prints all information (name, surname, ownership) about all owners to screen.
-            for (int i = 0; i < numberOfOwners; i++) {
-                cout << this->owners[i].getName() << " " << this->owners[i].getSurname() << " " << this->owners[i].get_ownership() << endl;
+            for (int i = 0; i < numberOfOwners; i++) { // stop if info about every Owner has been printed
+                cout << this->owners[i].getName() << " " << this->owners[i].getSurname() << " " << this->owners[i].get_ownership() << endl; // calls parent member functions to print
             }
         }
         int calculate_shipment_capacity() {
             //This method should sum up total transportation capacity.
-            Courier * traverse = couriers;
-            int totalShipmentCapacity = 0;
-            while (traverse != NULL) {
-                string vehicleType = traverse->getVehicleType();
+            Courier * traverse = couriers; // used to traverse the Courier Linked List
+            int totalShipmentCapacity = 0; // initalize capacity as zero
+            while (traverse != NULL) { // stop if end of Linked List is reached
+                string vehicleType = traverse->getVehicleType(); // store vehicle type as a variable since it will be used more than once
                 if (vehicleType.compare("car") == 0) {
-                    totalShipmentCapacity += 200;
+                    totalShipmentCapacity += 200; // if vehicle type is a car, add 200kg to the capacity
                 } else if (vehicleType.compare("motorcycle") == 0) {
-                    totalShipmentCapacity += 35;
+                    totalShipmentCapacity += 35; // if vehicle type is a motorcycle, add 35kg to the capacity
                 } else if (vehicleType.compare("bicycle") == 0) {
-                    totalShipmentCapacity += 10;
+                    totalShipmentCapacity += 10; // if vehicle type is a bicycle, add 10kg to the capacity
                 }
-                traverse = traverse->getNext();
+                traverse = traverse->getNext(); // check for the next courier in the list
             }
-            return totalShipmentCapacity;
+            return totalShipmentCapacity; // return the total calculated shipment capacity
         }
         void operator () () {
             // it should print all information about the business (i.e., name, address, owners, couriers).
-            cout << this->name << " " << this->address << endl;
-            list_owners();
-            list_couriers();
+            cout << this->name << " " << this->address << endl; // print name and address
+            list_owners(); // call the function for printing each owner of the business
+            list_couriers(); // call the function for printing each courier working for the business
         }
         Courier& operator [] (int index) {
             //it should return courier with the given index.
@@ -101,10 +103,11 @@ class Business {
             for (int i = 0; i < index; i++) {
                 if (traverse->getNext() == NULL) {
                     cout << "ERROR: Invalid courier index" << endl;
-                    return *traverse; // NEEDS A FIX
+                    exit(1); // the function expects an object to be returned
+                    // instead of returning a wrong object, exiting the program was chosen
                 }
-                traverse = traverse->getNext();
+                traverse = traverse->getNext(); // checks for the next courier in the list
             }
-            return *traverse;
+            return *traverse; // return the found courier
         }
 };
--- person.h
+++ person.h
@@ -4,67 +4,68 @@
 using namespace std;
 
 class Person {
-    string name;
-    string surname;
+    string name; // name of the Person 
+    string surname; // surname of the Person
     public:
         Person(string name, string surname) {
-            this->name = name;
-            this->surname = surname;
+            this->name = name; // sets name
+            this->surname = surname; // sets surname
         }
         string getName() {
-            return this->name;
+            return this->name; // getter for name
         }
         string getSurname() {
-            return this->surname;
+            return this->surname; // getter for surname
         }
 };
 
 class Owner : public Person {
-    int ownership; // as percentage
+    int ownership; // percentage of shares a person holds over a business
     public:
         Owner():Person("undefined", "undefined"){
-            //ownership = 0;
+            ownership = 0; // ownership will be updated during Business object construction
+            // default constructor used for owners array intialization
         }
         Owner(string name, string surname):Person(name,surname){
-            //ownership = 0;
+            ownership = 0; // ownership will be updated during Business object construction
         }
         void set_ownership(int percentage) {
-            this->ownership = percentage;
+            this->ownership = percentage; // updates ownership with calculated percentage by Business class constructor
         }
         int get_ownership() {
-            return ownership;
+            return ownership; // getter for ownership
         }
 };
 
 class Courier : public Person {
-    string vehicle; // car (200), motorcycle (35), bicycle (10)
-    Courier * next;
+    string vehicle; // type of vehicle courier uses (car, motorcycle or bicycle)
+    Courier * next; // next courier in the Linked List
     public:
         Courier(string name, string surname, string vehicle):Person(name,surname) {
             this->next = NULL;
             if (vehicle.compare("car") == 0 || vehicle.compare("motorcycle") == 0 || vehicle.compare("bicycle") == 0) {
-                this->vehicle = vehicle;
+                this->vehicle = vehicle; // set vehicle type if it is a valid input
             } else {
                 cout << "ERROR: Invalid Vehicle Type" << endl;
             }
         }
-        bool operator==(Courier& in_object) {
-            if (this->getSurname().compare(in_object.getSurname()) == 0) {
-                if (this->getName().compare(in_object.getName()) == 0) {
-                    if (this->vehicle.compare(in_object.vehicle) == 0) {
-                        return true;
+        bool operator==(Courier& in_object) { // const keywords were removed so parent class public member functions can be called
+            if (this->getSurname().compare(in_object.getSurname()) == 0) { // checks surname first because given main.cpp has all names as "Courier"
+                if (this->getName().compare(in_object.getName()) == 0) { // checks name as well just in case
+                    if (this->vehicle.compare(in_object.vehicle) == 0) { // checks vehicle type to make sure
+                        return true; // true if it is the one
                     }
                 }
             }
-            return false;
+            return false; // return false if they are not equal
         }
         Courier * getNext() {
-            return this->next;
+            return this->next; // getter for next Courier in the Linked List
         }
         void setNext(Courier * target) {
-            this->next = target;
+            this->next = target; // setter for the next Courier in the Linked List
         }
         string getVehicleType() {
-            return vehicle;
+            return vehicle; // getter for vehicle type
         }
 };
